5ea43797a26a6427276cdbe274898c553e256f04
TuongAnh <tuonganh147@gmail.com>
2023-08-02 09:23:58 +0700

    Update
    Update

---
+++ baiUI1/android/app/src/main/kotlin/com/example/baiui1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.baiui1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ baiui2/android/app/src/main/kotlin/com/example/baiui2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.baiui2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
